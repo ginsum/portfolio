08c24ecac3f572bb385f479d8f9d0e900567b802
immime12697@gmail.com <ginsum>
2020-04-01 01:47:31 +0900

    Fix react-quill toolbar

--- src/components/fragments/ContentInput.tsx
+++ src/components/fragments/ContentInput.tsx
@@ -2,6 +2,25 @@ import React from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
+const modules = {
+  toolbar: {
+    container: [
+      [{ header: "1" }, { header: "2" }],
+      ["bold", "italic", "underline", "strike"],
+      ["blockquote", "code-block"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ script: "sub" }, { script: "super" }], // superscript/subscript
+      [{ direction: "rtl" }], // text direction
+
+      [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ align: [] }]
+    ]
+  }
+};
+
 interface Props {
   content: string;
   setContent: any;
@@ -12,6 +31,7 @@ const ContentInput: React.FC<Props> = ({ content, setContent }) => {
     <>
       <ReactQuill
         value={content}
+        modules={modules}
         onChange={setContent}
         placeholder="내용을 입력하세요"
       />
